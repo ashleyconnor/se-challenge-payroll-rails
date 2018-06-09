806c0b2c87d7ca698c08013e67704346a4bf5b8d
Ashley Connor <ash.connor@gmail.com>
2018-06-09 18:39:19 -0400

    Add pay period calculator

--- app/helpers/report_helper.rb
+++ app/helpers/report_helper.rb
@@ -1,2 +1,9 @@
 module ReportHelper
+  def self.calculate_pay_period(date)
+    if (1..15).include?(date.day)
+      [date.beginning_of_month, date.change(day: 15)]
+    else
+      [date.change(day: 16), date.end_of_month]
+    end
+  end
 end
